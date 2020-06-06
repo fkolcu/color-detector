84934e301614519eafc38d54b86a855a9ed79319
Furkan <furkannkolcu@gmail.com>
2020-06-06 14:31:14 +0300

    full screen, catch button

--- app/src/main/java/com/furkank/colordetector/MainActivity.java
+++ app/src/main/java/com/furkank/colordetector/MainActivity.java
@@ -26,6 +26,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.nio.IntBuffer;
@@ -41,6 +43,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         // Define pointer
